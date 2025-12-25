6d8e60f917af4284423e23fb45d3adf652da71c0
MarioTurco <turcomario.na@gmail.com>
2025-12-25 17:58:27 +0100

    updated text in the UI;

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,13 @@ from utils import get_or_create_anon_id
 
 MAX_CHARACTERS = 200
 
-st.set_page_config(page_title="End of the year messages", page_icon="🎫")
-st.title("🎫 End of the year messages")
+st.set_page_config(page_title="New Year's resolution", page_icon="🎫")
+st.title("🎫 New Year's resolution")
 
 st.write(
     """
-    🎉 One New Year’s resolution. Totally anonymous.
-    Let’s see what everyone is aiming for this year!
+    🎉 Write your New Year’s resolution and join the collective goal-setting fun 🎊
+No accounts, no personal data — just anonymous insights and good vibes.
     """
 )
 
@@ -18,8 +18,8 @@ st.session_state.anon_id, st.session_state.new_user = get_or_create_anon_id()
 st.session_state.sb_conn = st.connection("supabase",type=SupabaseConnection)
 
 
-st.header("Write a new message")
+st.header("Write a New Year's resolution")
 
-txt = st.text_area(label="Message", max_chars=200)
+txt = st.text_area(label="New Year's resolution", max_chars=MAX_CHARACTERS, placeholder="Write your New Year's resolution")
 
-st.write(f"{len(txt)}/{MAX_CHARACTERS} characters.")
+
